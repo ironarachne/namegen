5f88fff5513f5a39a2f5ea60b631ff58fc7a1f02
Alexander Gaddy <34748779+alexgaddy@users.noreply.github.com>
2018-10-03 19:25:19 +0200

    Added German names

---
+++ germannames.go
@@ -0,0 +1,6 @@
+package namegen
+
+var (
+	germanFirstNames = []string{"Hans", "Wilhelm", "Gerhard", "Friedrich", "Karl", "Hildegard", "Lieselotte", "Ursula", "Elfriede", "Otto"}
+	germanLastNames = []string{"Mueller", "Schmidt", "Becker", "Wagner", "Fischer", "Bauer", "Richter", "Mayer", "Schneider", "Zimmermann"}
+)
