ce20e7bb89488dddd1bf336dbd866d4289778184
Sarai Díaz García <dgarciasarai@gmail.com>
2018-10-03 18:21:13 +0200

    Add spanish names

---
+++ spanishnames.go
@@ -0,0 +1,7 @@
+package namegen
+
+var (
+	spanishFirstNames = []string{"maría", "carmen", "antonio", "lucía", "hugo", "isabel", "pilar", "manuel", "juan", "javier"}
+	spanishLastNames = []string{"garcía", "gonzález", "fernández", "rodríguez", "lópez", "martínez", "sánchez", "pérez", "gómez", "martín"}
+)
+
