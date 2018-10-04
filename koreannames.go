50fdd3fd9f92b78f1e3612eadee62b2261a39537
Desi Cochrane <desi@desicochrane.com>
2018-10-04 10:29:19 +0200

    Add Korean names

---
+++ koreannames.go
@@ -0,0 +1,61 @@
+package namegen
+
+var (
+	koreanFirstNames = []string{
+		// female first names
+		"AeCha",
+		"BongCha",
+		"ChinSun", "ChoonHee", "ChunHei", "ChungCha", "ChungCha",
+		"Da", "Dae", "Du",
+		"Eui", "Eun", "EunAe", "EunJung", "EunKyung",
+		"Gi", "Goo",
+		"Hae", "HaeWon", "HeeYoung", "HeiRyung", "HeiRan", "HoSook", "Hoe", "HwaYoung", "Hyo", "HyoSonn", "Hyun", "HyunJae", "HyunAe", "HyunOk",
+		"JaeHwa", "Jee", "Ji", "JinAe", "JinKyong",
+		"Ki", "KyungMi", "KyungSoon", "KyungHu",
+		"Mee", "MiCha", "MiKyong", "MiSun", "MiYoung", "MiCha", "Mihi", "MiOk", "Min", "MinJee", "MinJung", "Mishil", "MunHee", "MyungHee", "MyungOk",
+		"SangHee", "SoYoung", "Soo", "SooJin", "SooMin", "SooYun", "Sook", "SoonBok", "SunHee", "SunJung", "SunHi", "Sung",
+		"Whan", "Woong",
+		"Yon", "Yoon", "YoungMi", "YoungIl", "YoungSoon", "Yun", "YunHee",
+
+		// male firstnames
+		"BaekHyeon", "BeomSeok", "Bitgaram",
+		"BonHwa",
+		"ChinHae", "ChinHwa", "ChinMae", "Chul", "ChulMoo", "ChungAe", "ChungHee", "ChungHo", "Courage",
+		"Da", "Dae", "DaeHo", "DaeHyun", "DaeJung", "Daeshim", "Dong", "DongMin", "DongSun", "DongYul", "DuHo",
+		"HakKun", "Haneul", "Hoon", "Hwan", "Hye", "Hyuk", "Hyun", "HyunKi", "HyunShik", "HyunKi", "HyunShik",
+		"Il", "In", "InSu", "Iseul",
+		"Jee", "Ji", "Jii", "Joon", "Jum", "Jung", "JungHwa",
+		"KangDae", "Ki", "Korain", "Kwan", "Kwang", "KwangSun", "Kyong", "Kyu", "Kyubok", "Kyung",
+		"MalChin", "ManShik", "ManYoung", "Myung", "MyungDae", "MyungSuck",
+		"Ryung",
+		"SangOok", "Seok", "Seung", "Shik", "So", "Suk",
+		"Tae",
+		"WonShik",
+		"Yongsun", "Young", "YoungJae", "YoungSoo",
+	}
+
+	koreanLastNames = []string{
+		"Ahn",
+		"Baek", "Bae", "Byun",
+		"Choi", "Cheon", "Chung", "Jung", "Joung", "Chong", "Cheong", "Cho", "Chu", "Choo", "Chee", "Chin", "Chen", "Chai", "Choun",
+		"Dho",
+		"Dong",
+		"Eum",
+		"Gi", "Gee", "Gin",
+		"Han", "Ho", "Hee", "Houng", "Hyong", "Hoang",
+		"Jung", "Jhang", "Joe", "Joh", "Jou", "Joo", "Jou", "Jee", "Jean",
+		"Kang", "Ko", "Kim", "Kong", "Ku", "Kwak", "Kwon",
+		"Lee", "Lim",
+		"Mihn", "Moon",
+		"Nahm", "Nho",
+		"Oh",
+		"Pahng", "Park",
+		"Rha", "Ryang", "Ryu",
+		"Soon", "Shim", "Shin", "Su", "Sung",
+		"Tcha", "Tu",
+		"Weon", "Wu",
+		"Yoon",
+		"Zhin",
+		"Wang", "Whang", "Wong",
+	}
+)
